c294434739c23c4af932178f95e8b2bdf50adfd2
Andrei Fedorov <4363626@gmail.com>
2024-12-14 17:27:23 -0800

    added comments in code

--- index.js
+++ index.js
@@ -2,46 +2,44 @@ const sharp = require('sharp');
 const fs = require('fs');
 const path = require('path');
 
-// Папка с исходными PNG изображениями
+// source folder with PNG images
 const inputFolder = 'src/img';
-// Папка для сохранения сконвертированных изображений
+// destination folder for converted images
 const outputFolder = 'dist/img';
 
-// Создание папки для вывода, если ее нет
+// create the source folder if it does not exist
 if (!fs.existsSync(outputFolder)) {
     fs.mkdirSync(outputFolder);
 }
 
-// Получение всех файлов из папки
+// list of images from source folder
 fs.readdir(inputFolder, (err, files) => {
     if (err) {
         console.error('Ошибка чтения папки:', err);
         return;
     }
 
-    // Фильтрация только PNG файлов
+    // select only PNG images
     const imgFiles = files.filter(file => path.extname(file).toLowerCase() === '.png');
 
-    // Проход по каждому PNG файлу
+    // for every PNG image
     imgFiles.forEach(file => {
         const filePath = path.join(inputFolder, file);
         const fileName = path.parse(file).name;
 
-        // Конвертация в JPG
+        // convert to JPG
         sharp(filePath)
             .jpeg()
             .toFile(path.join(outputFolder, `${fileName}.jpg`))
             .then(() => console.log(`${file} конвертирован в JPG`))
             .catch(err => console.error('Ошибка конвертации в JPG:', err));
-
-        // Конвертация в WebP
+        // convert to WebP
         sharp(filePath)
             .webp()
             .toFile(path.join(outputFolder, `${fileName}.webp`))
             .then(() => console.log(`${file} конвертирован в WebP`))
             .catch(err => console.error('Ошибка конвертации в WebP:', err));
-
-        // Конвертация в AVIF
+        // convert to AVIF
         sharp(filePath)
             .avif()
             .toFile(path.join(outputFolder, `${fileName}.avif`))
